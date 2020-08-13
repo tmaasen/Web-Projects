c31cab47cb67ce6c9bdc00caac4039aaae7d77e2
Tanner-Maasen <tmaasen@gmail.com>
2020-08-12 23:02:29 -0500

    removed Angular project

--- Angular/src/app/shared/shared.module.ts
+++
@@ -1,8 +0,0 @@
-import { NgModule } from '@angular/core';
-
-@NgModule({
-    declarations: [  ],
-    exports: [  ]
-})
-export class SharedModule { }
-// great place to put anything that is shared and will be used in multiple places
