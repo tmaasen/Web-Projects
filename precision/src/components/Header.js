b0941ca5ff887d69ca990585cce1a12617c6f769
Tanner-Maasen <tmaasen@gmail.com>
2021-04-08 23:42:55 -0500

    removed precision app

--- precision/src/components/Header.js
+++
@@ -1,9 +0,0 @@
-import React from 'react'
-
-export const Header = () => {
-    return (
-        <div className="title">
-            <h1 className="header">React Export To Excel Example</h1>
-        </div>
-    )
-}
